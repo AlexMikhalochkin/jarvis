89b35d2f90f252e89394255ee35be359b293697f
AlexMikhalochkin <alexmikhalochkin@gmail.com>
2024-09-03 12:26:21 +0300

    Bump gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.5.2"
+    id("org.springframework.boot") version "2.7.1"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("jacoco")
     id("io.gitlab.arturbosch.detekt") version "1.19.0"
-    kotlin("jvm") version "1.5.20"
-    kotlin("plugin.spring") version "1.5.20"
+    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.spring") version "1.7.10"
     id("com.diffplug.spotless") version "5.14.2"
     id("org.openapi.generator") version "5.1.1"
 }
