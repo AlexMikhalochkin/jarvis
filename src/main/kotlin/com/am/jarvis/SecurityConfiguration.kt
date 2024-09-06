7d4ac8f072cd2cdc7f24da8cddd586676d1456b1
AlexMikhalochkin <alexmikhalochkin@gmail.com>
2024-09-06 21:48:16 +0300

    Disable HTTP basic

--- src/main/kotlin/com/am/jarvis/SecurityConfiguration.kt
+++ src/main/kotlin/com/am/jarvis/SecurityConfiguration.kt
@@ -2,7 +2,6 @@ package com.am.jarvis
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
@@ -19,7 +18,6 @@ class SecurityConfiguration {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         return http.csrf { it.disable() }
-            .httpBasic(Customizer.withDefaults())
             .authorizeHttpRequests { request ->
                 request.requestMatchers("/yandex/v1.0/**", "/smartthings")
                     .permitAll()
