046b4fd8038ffbb45b03f7e8c5dcf8c839579184
AlexMikhalochkin <alexmikhalochkin@gmail.com>
2025-01-24 12:08:39 +0300

    Refactor yandex notifier

--- momomo-notifier-yandex/src/main/kotlin/com/am/jarvis/connector/yandex/notifier/YandexNotifier.kt
+++ momomo-notifier-yandex/src/main/kotlin/com/am/jarvis/connector/yandex/notifier/YandexNotifier.kt
@@ -27,64 +27,48 @@ class YandexNotifier(
 
     override fun notify(states: List<DeviceState>, flag: Boolean) {
         logger.info { "Yandex. Send notification. Started. States=$states" }
-        val requestPayload = if (flag) createRequest(states[0]) else createRequest2(states[0])
+        val changeStateDevice = if (flag) createRequest(states[0]) else createRequest2(states[0])
+        val payload = NotificationPayload(userId, listOf(changeStateDevice))
+        val requestPayload = YandexNotificationRequest(payload, getCurrentTimestamp())
         yandexApiClient.notify(requestPayload)
         logger.info { "Yandex. Send notification. Finished. States=$states, RequestPayload=$requestPayload" }
     }
 
     override fun getSource() = "YANDEX"
 
-    private fun createRequest(state: DeviceState): YandexNotificationRequest {
+    private fun createRequest(state: DeviceState): ChangeStatesDevice {
         val fullCapability = FullCapability(
             YandexState("on", state.isOn),
             "devices.capabilities.on_off"
         )
-        val changeStateDevice = ChangeStatesDevice(
+        return ChangeStatesDevice(
             state.deviceId,
             listOf(fullCapability),
             null
         )
-        val payload = NotificationPayload(
-            userId,
-            listOf(changeStateDevice)
-        )
-        return YandexNotificationRequest(payload, getCurrentTimestamp())
     }
 
-    private fun createRequest2(state: DeviceState): YandexNotificationRequest {
-        val property = property(state, "temperature", "temperature")
-        val property2 = property(state, "humidity", "humidity")
-        val property3 = property(state, "battery_level", "battery")
-        val property4 = property(state, "voltage", "voltage")
-        val property5 = property2(state)
-        val changeStateDevice = ChangeStatesDevice(
+    private fun createRequest2(state: DeviceState): ChangeStatesDevice {
+        val property = createFloatProperty(state, "temperature", "temperature")
+        val property2 = createFloatProperty(state, "humidity", "humidity")
+        val property3 = createFloatProperty(state, "battery_level", "battery")
+        val property4 = createFloatProperty(state, "voltage", "voltage")
+        val property5 = createProperty(state, "button", "button", "devices.properties.event")
+        return ChangeStatesDevice(
             state.deviceId,
             listOf(),
             listOfNotNull(property, property2, property3, property4, property5)
         )
-        val payload = NotificationPayload(
-            userId,
-            listOf(changeStateDevice)
-        )
-        return YandexNotificationRequest(payload, getCurrentTimestamp())
     }
 
-    private fun property(state: DeviceState, instance: String, customData: String): Property? {
-        val any = state.customData[customData] ?: return null
-        val float = (any as Int).toFloat()
-        return Property(
-            YandexState(instance, float),
-            "devices.properties.float"
-        )
+    private fun createProperty(state: DeviceState, instance: String, customData: String, type: String): Property? {
+        val value = state.customData[customData] ?: return null
+        val yandexState = YandexState(instance, value)
+        return Property(yandexState, type)
     }
 
-    private fun property2(state: DeviceState): Property? {
-        val any = state.customData["button"] ?: return null
-        val float = any as String
-        return Property(
-            YandexState("button", float),
-            "devices.properties.event"
-        )
+    private fun createFloatProperty(state: DeviceState, instance: String, customData: String): Property? {
+        return createProperty(state, instance, customData, "devices.properties.float")
     }
 
     @Suppress("MagicNumber")
