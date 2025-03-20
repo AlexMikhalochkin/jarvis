a9fe493e02e4bf61ac49ca834a6a2f14394467fb
AlexMikhalochkin <alexmikhalochkin@gmail.com>
2025-03-20 12:51:40 +0300

    Add all devices to the dashboard

--- momomo-worker-app/src/main/kotlin/com/am/jarvis/DashboardController.kt
+++ momomo-worker-app/src/main/kotlin/com/am/jarvis/DashboardController.kt
@@ -1,26 +1,25 @@
 package com.am.jarvis
 
+import com.am.jarvis.core.api.SmartHomeService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class DashboardController {
+class DashboardController(
+    val smartHomeService: SmartHomeService
+) {
 
     @GetMapping("/")
     fun getDashboard(model: Model): String {
-        val bulbs = listOf(
-            Bulb(
-                1,
-                "Bulb 1"
-            )
-        )
+        val bulbs = smartHomeService.getAllDevices()
+            .map { Bulb(it.id, it.name.primaryName) }
         model.addAttribute("bulbs", bulbs)
         return "index"
     }
 
     data class Bulb(
-        val id: Int,
+        val id: String,
         val name: String
     )
 }
