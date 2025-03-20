a7bdd3367b457591097ada9a49cca459335fb2ea
AlexMikhalochkin <alexmikhalochkin@gmail.com>
2025-03-20 14:21:39 +0300

    Add endpoint to change states from the dashboard

--- momomo-worker-app/src/main/kotlin/com/am/jarvis/DashboardController.kt
+++ momomo-worker-app/src/main/kotlin/com/am/jarvis/DashboardController.kt
@@ -1,13 +1,20 @@
 package com.am.jarvis
 
 import com.am.jarvis.core.api.DeviceProvider
+import com.am.jarvis.core.api.DeviceStateChanger
+import com.am.jarvis.core.model.DeviceState
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class DashboardController(
-    val deviceProviders: List<DeviceProvider>
+    val deviceProviders: List<DeviceProvider>,
+    val deviceStateChanger: DeviceStateChanger
 ) {
 
     @GetMapping("/")
@@ -16,6 +23,12 @@ class DashboardController(
             .map { Bulb(it.id, it.name.primaryName) }
         model.addAttribute("bulbs", bulbs)
         return "index"
+    }
+
+    @PostMapping("/bulbs/{id}/toggle")
+    fun changeState(@PathVariable id: String, @RequestParam state: Boolean): ResponseEntity<String> {
+        deviceStateChanger.changeStates(listOf(DeviceState(id, state)))
+        return ResponseEntity.ok("OK")
     }
 
     data class Bulb(
