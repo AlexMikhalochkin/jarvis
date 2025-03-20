589f929b0a2a24f9c774fa2939a97d249a37c90e
AlexMikhalochkin <alexmikhalochkin@gmail.com>
2025-03-20 12:59:45 +0300

    Fix provision of devices for the dashboard

--- momomo-worker-app/src/main/kotlin/com/am/jarvis/DashboardController.kt
+++ momomo-worker-app/src/main/kotlin/com/am/jarvis/DashboardController.kt
@@ -1,18 +1,18 @@
 package com.am.jarvis
 
-import com.am.jarvis.core.api.SmartHomeService
+import com.am.jarvis.core.api.DeviceProvider
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class DashboardController(
-    val smartHomeService: SmartHomeService
+    val deviceProviders: List<DeviceProvider>
 ) {
 
     @GetMapping("/")
     fun getDashboard(model: Model): String {
-        val bulbs = smartHomeService.getAllDevices()
+        val bulbs = deviceProviders.flatMap { it.getAllDevices() }
             .map { Bulb(it.id, it.name.primaryName) }
         model.addAttribute("bulbs", bulbs)
         return "index"
